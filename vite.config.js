ae70e9dc86f3469ddd9105bdd2edaa03ab390a20
PrayagN <Prayag.n.manojnair@gmail.com>
2023-10-02 14:36:15 +0530

    added assetsdir

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base:'/weatherWeb/',
+  assetsDir: 'assets',
   preview :{
     host :true,
     port:1000
